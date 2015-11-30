5fa7058560cc05169879af2236424035950242b7
Ben Laurie <ben@links.org>
2015-11-30 13:58:11 +0000

    Remove tests that are already in common_json.test.

--- src/test/java/org/links/objecthash/ObjectHashTest.java
+++ src/test/java/org/links/objecthash/ObjectHashTest.java
@@ -26,49 +26,6 @@ public class ObjectHashTest {
   }
 
   @Test
-  public void testEmptyList() throws Exception {
-    runTest("[]",
-        "acac86c0e609ca906f632b0e2dacccb2b77d22b0621f20ebece1a4835b93f6f0");
-  }
-
-  @Test
-  public void testListOneString() throws Exception {
-    runTest("[\"foo\"]",
-        "268bc27d4974d9d576222e4cdbb8f7c6bd6791894098645a19eeca9c102d0964");
-  }
-
-  @Test
-  public void testListTwoStrings() throws Exception {
-    runTest("[\"foo\", \"bar\"]",
-        "32ae896c413cfdc79eec68be9139c86ded8b279238467c216cf2bec4d5f1e4a2");
-  }
-
-  @Test
-  public void testEmptyObject() throws Exception {
-    runTest("{}",
-        "18ac3e7343f016890c510e93f935261169d9e3f565436429830faf0934f4f8e4");
-  }
-
-  @Test
-  public void testListObjectOneStringKeyValue() throws Exception {
-    runTest("{\"foo\": \"bar\"}\"",
-        "7ef5237c3027d6c58100afadf37796b3d351025cf28038280147d42fdc53b960");
-  }
-
-  @Test
-  public void testObjectWithListsOfStrings() throws Exception {
-    runTest("{\"foo\": [\"bar\", \"baz\"], \"qux\": [\"norf\"]}",
-        "f1a9389f27558538a064f3cc250f8686a0cebb85f1cab7f4d4dcc416ceda3c92");
-
-  }
-
-  @Test
-  public void testNull() throws Exception {
-    runTest("[null]",
-        "5fb858ed3ef4275e64c2d5c44b77534181f7722b7765288e76924ce2f9f7f7db");
-  }
-
-  @Test
   public void test32BitIntegers() throws Exception {
     runTest("[123]",
         "1b93f704451e1a7a1b8c03626ffcd6dec0bc7ace947ff60d52e1b69b4658ccaa");
