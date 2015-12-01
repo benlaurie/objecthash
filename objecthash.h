40f67931d7f784a1ff01488e8840b18d027721f7
Ben Laurie <ben@links.org>
2015-12-01 14:41:16 +0000

    Undo Al's damage.

--- objecthash.h
+++ objecthash.h
@@ -13,6 +13,7 @@ static const int HASH_SIZE = SHA256_DIGEST_LENGTH;
 typedef byte hash[HASH_SIZE];
 typedef SHA256_CTX hash_ctx;
 
+bool python_json_hash(const char *json, hash h);
 bool common_json_hash(const char *json, hash h);
 
 #ifdef __cplusplus
